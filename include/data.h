402834f950d5dc58a11da8ad0a7e5d8c94bd031d
JYMPCB <jympcb@yahoo.com.ar>
2024-11-12 07:56:41 -0300

    first commit

---
+++ include/data.h
@@ -0,0 +1,8 @@
+//const char* nombre = "willi-66";
+String Pagina = R"====(
+<form method='POST' action='/actualizar' enctype='multipart/form-data'>
+<input type='file' name='update'>
+<br>
+<input type='submit' value='actualizar'>
+</form>
+)====";
---
+++ include/dataWilli.h
@@ -0,0 +1,2 @@
+const char* version = "V3.8";
+bool rgb = 1;     //0 = sin RGB, 1 = con RGB
