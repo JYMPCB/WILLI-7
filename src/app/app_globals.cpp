f04a6c7716011c8396a6ff7ab8dfd962a1ed6cb4
JYMPCB <jympcb@yahoo.com.ar>
2026-01-06 14:39:17 -0300

    OTA

--- src/app/app_globals.cpp
+++ src/app/app_globals.cpp
@@ -4,7 +4,7 @@
 
 
 // --- Firmware version ---
-const char* g_fw_version = "1.0.0";
+const char* g_fw_version = "1.0.1";
 const uint32_t g_fw_build = 20260106;
 // --- OTA state ---
 volatile bool g_ota_available = false;
